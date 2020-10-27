eb4c8afb3ff0090d246203d9b83eb200b3a10b40
박주영 <juyoung7018.park@gmail.com>
2020-10-27 18:29:10 +0900

    chore: 프로젝트 setting
    - Server, client에 util 폴더 추가
    - db-config.js에 pool 생성 후 export

---
+++ src/server/config/db-config.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
+});
+
+module.exports = pool.promise();
