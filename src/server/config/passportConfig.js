7025876e4350c56876a84b35ca3d080f998a5ed2
박주영 <juyoung7018.park@gmail.com>
2020-10-28 21:56:20 +0900

    feat: passport local, jwt 전략 추가
    - 인증 수단에서 local, jwt 전략을 추가해서 인증을 진행할 수 있게 함

---
+++ src/server/config/passportConfig.js
@@ -0,0 +1,46 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const userService = require('../services/userService');
+
+const JWTStrategy = passportJWT.Strategy;
+const extractJWT = passportJWT.ExtractJwt;
+
+const initPassport = () => {
+  const JWTOption = {
+    jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  };
+
+  const localOption = {
+    usernameField: 'userName',
+    passwordField: 'password',
+    session: false,
+  };
+
+  passport.use(
+    new LocalStrategy(localOption, async (userName, password, done) => {
+      try {
+        if (!userName) return done(null, undefined);
+        const user = await userService.findUser(userName);
+        if (!user) return done(null, undefined);
+        if (user && user.password === password) return done(null, user);
+        return done(null, undefined);
+      } catch (err) {
+        return done(err, undefined);
+      }
+    }),
+  );
+
+  passport.use(
+    new JWTStrategy(JWTOption, (payload, done) => {
+      const { user } = payload;
+      if (!user) return done(null, undefined);
+      return done(null, user);
+    }),
+  );
+
+  return passport.initialize();
+};
+
+module.exports = initPassport;
