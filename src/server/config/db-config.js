1f5f5f9ae1b390de807d600b1555a92fd65dd8ce
chahtk <chahtk@gmail.com>
2020-10-28 19:16:32 +0900

    fix: 테스트용 코드 제거
    - 처음, 테스트를 하려면 지워진 코드를 사용해야 하는줄 알았다.
    - 그러나 테스트 코드에서 dotenv를 호출하면 되더라,,,

--- src/server/config/db-config.js
+++ src/server/config/db-config.js
@@ -1,10 +1,5 @@
 const mysql = require('mysql2');
 
-if (process.env.NODE_ENV === 'test') {
-  // eslint-disable-next-line global-require
-  require('dotenv').config();
-}
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
