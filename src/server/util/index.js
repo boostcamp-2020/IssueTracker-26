2b33cccc36699ec3424a570f4a7ecd3b2b98c532
박주영 <juyoung7018.park@gmail.com>
2020-10-29 17:56:28 +0900

    feat: Oauth 로그인 시 사용할 randomString 함수 추가
    - 로그인 시에 유저 테이블에 존재하지않는 경우 추가해야함
    - 추가하는 경우 비밀번호를 랜덤하게 생성해서 넣어줌

--- src/server/util/index.js
+++ src/server/util/index.js
@@ -5,6 +5,14 @@ const makeToken = (payload) => {
   return token;
 };
 
+const randomString = () => {
+  return (
+    Math.random().toString(36).substring(2) +
+    Math.random().toString(36).substring(2)
+  );
+};
+
 module.exports = {
   makeToken,
+  randomString,
 };
