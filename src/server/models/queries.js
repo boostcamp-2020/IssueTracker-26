4933f5d20dd6681c3ac40d7d7692e6203754ad06
Yi SoJeong <soj980312@gmail.com>
2020-10-29 11:14:03 +0900

    feat: label 생성 쿼리 추가
    - label을 생성하기 위한 SQL문

--- src/server/models/queries.js
+++ src/server/models/queries.js
@@ -2,6 +2,11 @@ const USER = {
   SIGNUP: `INSERT INTO user(userName, password) VALUES(?,?)`,
 };
 
+const LABEL = {
+  CREATE: `INSERT INTO label(title, description, color) VALUES(?,?,?)`,
+};
+
 module.exports = {
   USER,
+  LABEL,
 };
