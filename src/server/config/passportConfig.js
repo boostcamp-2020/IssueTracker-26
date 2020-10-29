feed5f607e9492b3477a6d2ded8c21b8353fede9
박주영 <juyoung7018.park@gmail.com>
2020-10-29 17:54:54 +0900

    feat: Oauth GitHub 로그인을 위한 strategy 추가
    - github 로그인을 위한 strategy 추가
    - serializeUser, deserializeUser로 profile을 session에 저장

--- src/server/config/passportConfig.js
+++ src/server/config/passportConfig.js
@@ -1,12 +1,21 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const passportJWT = require('passport-jwt');
+const GitHubStrategy = require('passport-github2').Strategy;
 const userService = require('../services/userService');
 
 const JWTStrategy = passportJWT.Strategy;
 const extractJWT = passportJWT.ExtractJwt;
 
 const initPassport = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((obj, done) => {
+    done(null, obj);
+  });
+
   const JWTOption = {
     jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SECRET,
@@ -38,6 +47,22 @@ const initPassport = () => {
       if (!user) return done(null, undefined);
       return done(null, user);
     }),
+  );
+
+  const GitHubOption = {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: `${process.env.HOST}/api/auth/github/callback`,
+  };
+
+  passport.use(
+    new GitHubStrategy(
+      GitHubOption,
+      (accessToken, refreshToken, profile, done) => {
+        const { username } = profile;
+        done(null, username);
+      },
+    ),
   );
 
   return passport.initialize();
