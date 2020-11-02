c7179ad64c4f5f21c5d6f352816bc971026ed5c5
WooDong <tmlu48@naver.com>
2020-11-02 17:50:19 +0900

    fix: 이슈 활성화 API TEXT 코드 수정

--- src/server/tests/issue.test.js
+++ src/server/tests/issue.test.js
@@ -187,16 +187,17 @@ describe('POST /issue는', () => {
   })
 })
 
-/* 이슈 닫기 API 테스트*/
+/* 이슈 활성화 API 테스트*/
 /* 단위 테스트 */
-describe('이슈 닫기 API 단위 TEST', () => {
+describe('이슈 활성화 API 단위 TEST', () => {
+  const userId = 1;
   const onState = 1;
   const closeState = 0;
-  describe('이슈 닫기 MODEL API TEST', () => {
+  describe('이슈 활성화 MODEL API TEST', () => {
     describe('이슈 MODEL', ()=>{
   
       test('사용자가 이슈 닫기', async () => {
-        const data = await issueModel.stateChangeIssue(onState);
+        const data = await issueModel.stateChange(onState, userId);
         expect(data).toBeDefined();
       })
     })
@@ -204,17 +205,17 @@ describe('이슈 닫기 API 단위 TEST', () => {
     describe('이슈 MODEL', ()=>{
   
       test('사용자가 이슈 활성화하기', async () => {
-        const data = await issueModel.stateChangeIssue(closeState);
+        const data = await issueModel.stateChange(closeState, userId);
         expect(data).toBeDefined();
       })
     })
   })
   
-  describe('이슈 닫기 SERVICE API TEST', ()=>{
+  describe('이슈 활성화 SERVICE API TEST', ()=>{
     describe('이슈 SERVICE', ()=>{
   
       test('사용자가 이슈 닫기', async () => {
-        const data = await issueService.stateChangeIssue(onState);
+        const data = await issueService.stateChange(onState, userId);
         expect(data).toBeDefined();
       })
     })
@@ -222,7 +223,7 @@ describe('이슈 닫기 API 단위 TEST', () => {
     describe('이슈 SERVICE', ()=>{
   
       test('사용자가 이슈 활성화하기', async () => {
-        const data = await issueService.stateChangeIssue(closeState);
+        const data = await issueService.stateChange(closeState, userId);
         expect(data).toBeDefined();
       })
     })
@@ -231,7 +232,7 @@ describe('이슈 닫기 API 단위 TEST', () => {
 
 /* 슈퍼 테스트 */
 describe('POST /issue는', () => {
-  const state = {state: 1};
+  const state = {state: 1, id: 1};
   describe('성공시', () => {
 
     test('이슈를 성공적으로 닫으면 200을 반환한다.', async (done) => {
