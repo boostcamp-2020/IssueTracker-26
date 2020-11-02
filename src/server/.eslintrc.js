aa5d36fea1d574e992011281d3a05dde6ead9335
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-02 23:25:35 +0900

    chore: eslint 옵션에서 emcaVersion 2017 -> 2018로 바꿈
    - spread object는 18부터 지원되서 eslint 옵션을 바꿔줬음.

--- src/server/.eslintrc.js
+++ src/server/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es6: true,
   },
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
   },
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   rules: {
