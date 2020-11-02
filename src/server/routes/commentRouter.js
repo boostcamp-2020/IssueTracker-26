c27f4a31135930299ae2a3e0495b9b440a0eccc5
chahtk <chahtk@gmail.com>
2020-11-02 21:22:28 +0900

    feat: comment 삭제 controller
    - id가 숫자가 아니면 400에러
    - 삭제 성공시 205응답
    - id가 숫자지만 db에 없을때 406에러

--- src/server/routes/commentRouter.js
+++ src/server/routes/commentRouter.js
@@ -3,5 +3,6 @@ const commentController = require('../controllers/commentController');
 
 router.post('/comment', commentController.create);
 router.get('/comment', commentController.read);
+router.delete('/comment/:id', commentController.remove);
 
 module.exports = router;
