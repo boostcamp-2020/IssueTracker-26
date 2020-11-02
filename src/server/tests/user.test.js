a757320dff70d4f9b34eb335c965b0a3c535a8a0
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-02 15:26:53 +0900

    test: 비밀번호 암호화 테스트 작성
    - 비밀번호 암호화 함수와 비밀번호 비교 함수 테스트 작성

--- src/server/tests/user.test.js
+++ src/server/tests/user.test.js
@@ -2,6 +2,21 @@ require('dotenv').config();
 /* eslint-disable */
 const userService = require('../services/userService');
 const userController = require('../controllers/userController');
+const { makeHashPassword, comparePassword } = require('../util');
+
+describe('비밀번호 암호화 테스트(makeHashPassword, comparePassword)', () => {
+  const password = '12345';
+  const hashedPassword = makeHashPassword(password);
+  test('비밀번호와 암호화된 비밀번호를 비교했을 때, true여야 한다.', () => {
+    const isSamePassword = comparePassword(password, hashedPassword);
+    expect(isSamePassword).toBe(true);
+  });
+  test('암호회된 비밀번호가 다른 비밀번호랑 비교했을때, false여야 한다.', () => {
+    const otherPassword = '54321';
+    const isSamePassword = comparePassword(otherPassword, hashedPassword);
+    expect(isSamePassword).toBe(false);
+  });
+});
 
 describe('userService 테스트', () => {
   describe('findUser', () => {
