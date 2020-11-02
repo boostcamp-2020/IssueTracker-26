80f64adbab71fa3bed2f7ff0bbc21a484d1bbcd8
WooDong <tmlu48@naver.com>
2020-11-03 03:48:54 +0900

    test: 이슈 내용 수정하기 Test

--- src/server/tests/issue.test.js
+++ src/server/tests/issue.test.js
@@ -260,11 +260,20 @@ const issueService = require('../services/issueService');
 describe('이슈 제목 수정하기 API 단위 TEST', () => {
   const id = 1;
   const title = 'test 제목';
+  const content = 'test 내용';
   describe('이슈 제목 수정하기 MODEL API TEST', () => {
     describe('이슈 MODEL', ()=>{
   
       test('이슈 제목 수정하기', async () => {
         const data = await issueModel.titleUpdate(id, title);
+        expect(data).toBeDefined();
+      })
+    })
+
+    describe('이슈 MODEL', ()=>{
+  
+      test('이슈 내용 수정하기', async () => {
+        const data = await issueModel.contentUpdate(id, content);
         expect(data).toBeDefined();
       })
     })
@@ -275,6 +284,16 @@ describe('이슈 제목 수정하기 API 단위 TEST', () => {
   
       test('이슈 제목 수정하기', async () => {
         const data = await issueService.titleUpdate(id, title);
+        expect(data).toBeDefined();
+      })
+    })
+  })
+
+  describe('이슈 내용 수정하기 SERVICE API TEST', ()=>{
+    describe('이슈 SERVICE', ()=>{
+  
+      test('이슈 내용 수정하기', async () => {
+        const data = await issueService.contentUpdate(id, content);
         expect(data).toBeDefined();
       })
     })
@@ -302,6 +321,32 @@ describe('PUT /issue/title은', () => {
       info.title='';
       const response = await request(app)
       .put('/api/issue/title/1').send(info);
+      expect(response.status).toEqual(400);
+      done();
+    })
+  })
+})
+
+describe('PUT /issue/content는', () => {
+  const info = {
+    "content" : "테스트 내용"
+  }
+  describe('성공시', () => {
+
+    test('이슈 내용을 성공적으로 수정하면 200을 반환한다.', async (done) => {
+      const response = await request(app)
+      .put('/api//issue/content/1').send(info);
+      expect(response.status).toEqual(200);
+      done();
+    })
+  })
+  
+  describe('실패시', () => {
+    
+    test('이슈 내용이 존재하지 않으면 400을 반환한다.', async (done) => {
+      info.content='';
+      const response = await request(app)
+      .put('/api//issue/content/1').send(info);
       expect(response.status).toEqual(400);
       done();
     })
