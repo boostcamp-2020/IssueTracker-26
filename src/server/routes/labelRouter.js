3ffc7c0f2fe9c3071c3b31ab84b564774e14437e
Yi SoJeong <soj980312@gmail.com>
2020-11-02 16:28:59 +0900

    feat: Label 목록 읽어오는 api 연결
    - 읽어오는 api 이므로 get 요청

--- src/server/routes/labelRouter.js
+++ src/server/routes/labelRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const labelController = require('../controllers/labelController');
 
 router.post('/label', labelController.createLabel);
+router.get('/label', labelController.getLabelList);
 
 module.exports = router;
