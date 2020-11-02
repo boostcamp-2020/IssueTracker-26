5d26e5f333bab2945933466d09b7b8ce0a763e7d
chahtk <chahtk@gmail.com>
2020-11-02 15:52:33 +0900

    feat: 멘션된 유저가 실제로 있는지 확인하는 함수
    - '@park' 가 왔을 때 park가 실제 user인지 체크하고 userId리턴
    - 입력은 배열 형태로 오기 때문에 멘션 여러개가 올 수 있음

--- src/server/services/commentService.js
+++ src/server/services/commentService.js
@@ -1,4 +1,6 @@
+/* eslint-disable array-callback-return */
 const commentModel = require('../models/commentModel');
+const userModel = require('../models/userModel');
 
 const containMention = (content) => {
   const reg = /(@[\w]{6,16})/g;
@@ -6,9 +8,36 @@ const containMention = (content) => {
   return mentions;
 };
 
+const checkUser = async (mentions) => {
+  // select id from user where userName=${mention}
+  const userId = [];
+  const promiseList = [];
+
+  mentions.forEach((mention) => {
+    const userName = mention.replace('@', '');
+    const resultUser = userModel.checkDuplicated(userName);
+    promiseList.push(resultUser);
+  });
+
+  await Promise.all(promiseList).then((items) => {
+    items.map((item) => {
+      if (item) {
+        userId.push(item.id);
+      }
+    });
+  });
+  return userId;
+};
+
+const createMention = (userId, issueId, commentId = null) => {
+  console.log(commentId);
+};
+
 const create = async ({ content, userId, issueId }) => {
   try {
     const commentId = await commentModel.create({ content, userId, issueId });
+    // const mentions = containMention(content);
+
     return commentId;
   } catch (err) {
     return undefined;
@@ -18,4 +47,6 @@ const create = async ({ content, userId, issueId }) => {
 module.exports = {
   create,
   containMention,
+  checkUser,
+  createMention,
 };
