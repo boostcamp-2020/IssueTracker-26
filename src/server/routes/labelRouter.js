c2688c7265976177e53af3bf780cdfe396b760b6
Yi SoJeong <soj980312@gmail.com>
2020-11-02 23:33:57 +0900

    feat: Label 수정 라우터 연결

--- src/server/routes/labelRouter.js
+++ src/server/routes/labelRouter.js
@@ -3,5 +3,6 @@ const labelController = require('../controllers/labelController');
 
 router.post('/label', labelController.createLabel);
 router.get('/label', labelController.getLabelList);
+router.put('/label/:id', labelController.updateLabel);
 
 module.exports = router;
