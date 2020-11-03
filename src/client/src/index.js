57abb94d0dd17e5f16501b09fc24ed3a763f7e89
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-03 19:55:49 +0900

    chore: React 구조 세팅
    - styled-components 설치
    - pages폴더 추가

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
 ReactDOM.render(
-        <App/>,
-    document.getElementById('root')
-)
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
