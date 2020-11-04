2d0ad4fa6c1b69bf920a24a6fb9a7f9aaec14bf2
Yi SoJeong <soj980312@gmail.com>
2020-11-04 18:47:54 +0900

    feat: Page 추가
    - label, milestone 페이지 추가

--- src/client/src/components/app.js
+++ src/client/src/components/app.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import LoginPage from '../pages/LoginPage';
+import LabelPage from '../pages/LabelPage';
+import MilestonePage from '../pages/MilestonePage';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={MainPage} />
       <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/label" component={LabelPage} />
+      <Route exact path="/milestone" component={MilestonePage} />
       <Route />
     </Switch>
   );
