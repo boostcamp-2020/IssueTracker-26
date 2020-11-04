66c878d8fc3cea3209bd8bec62bac3b7c60bbe38
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-04 14:18:46 +0900

    chore: GlobalStyle, Theme 추가

---
+++ src/client/src/components/Theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  Color: {
+    inputShadow: '#bbd1f3',
+    inputOutline: '#0466d5',
+    border: '#cfd3da',
+    grayBackground: '#f7f8fa',
+  },
+};
+
+export default theme;
--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './components/App';
+import GlobalStyle from './components/GlobalStyle';
+import Theme from './components/Theme';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={Theme}>
+    <BrowserRouter>
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
