e0d9fe95e4e85d86cf396855b82b3b9b49f5268c
WooDong <tmlu48@naver.com>
2020-11-06 05:49:23 +0900

    feat: 라벨 총 갯수 구하기 API Controller 구현

--- src/client/src/pages/MainPage.js
+++ src/client/src/pages/MainPage.js
@@ -2,7 +2,12 @@ import React from 'react';
 import Header from '../components/header/Header';
 
 function MainPage() {
-  return <div><Header/></div>;
+  return (
+    <div>
+      <Header />
+    
+    </div>
+  );
 }
 
 export default MainPage;
