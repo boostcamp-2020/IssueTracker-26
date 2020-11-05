a885bf82de9f0dfc2223b3bd8ac832b4d2748537
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-05 18:28:40 +0900

    feat: User context 추가, App에서 UserContextProvider 추가
    - isLoggedIn, token을 context로 관리

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import LoginPage from '../pages/LoginPage';
 import LabelPage from '../pages/LabelPage';
 import MilestonePage from '../pages/MilestonePage';
+import UserContext from './Context/UserContext';
 
 function App() {
+  const [state, setState] = useState({
+    token: localStorage.getItem('jwt'),
+    isLoggedIn: !!localStorage.getItem('jwt'),
+  });
+  const { isLoggedIn } = state;
+  return (
+    <UserContext.Provider
+      value={{
+        state: { ...state },
+        setState,
+      }}
+    >
+      {isLoggedIn ? <PrivateRoute /> : <PublicRoute />}
+    </UserContext.Provider>
+  );
+}
+
+function PublicRoute() {
+  return (
+    <Switch>
+      <Route path="/login" component={LoginPage} />
+      <Route path="/">
+        <Redirect to="/login" />
+      </Route>
+    </Switch>
+  );
+}
+
+function PrivateRoute() {
   return (
     <Switch>
       <Route exact path="/" component={MainPage} />
-      <Route exact path="/login" component={LoginPage} />
       <Route exact path="/label" component={LabelPage} />
       <Route exact path="/milestone" component={MilestonePage} />
-      <Route />
+      <Route exact path="/login">
+        <Redirect to="/" />
+      </Route>
     </Switch>
   );
 }
