9c6716cca81f9c59c26547b43c68d3fd440eb2c2
chahtk <chahtk@gmail.com>
2020-11-05 15:08:56 +0900

    style: 경고 스타일 추가(인풋)

--- src/client/src/components/SharedStyle.js
+++ src/client/src/components/SharedStyle.js
@@ -2,11 +2,22 @@ import { css } from 'styled-components';
 
 const ShardStyle = {};
 
+const sharedInputOutlineStyle = css`
+  outline-style: none;
+  border: 0.1px solid ${(props) => props.theme.Color.inputBorder};
+`;
+
 ShardStyle.inputOutlineStyle = css`
   &:focus {
     box-shadow: 0 0 0 4px ${(props) => props.theme.Color.inputShadow};
-    outline-style: none;
-    border: 0.1px solid ${(props) => props.theme.Color.inputOutline};
+    ${sharedInputOutlineStyle}
+  }
+`;
+
+ShardStyle.warningOutlineStyle = css`
+  &:focus {
+    box-shadow: 0 0 0 4px ${(props) => props.theme.Color.warningInputShadow};
+    ${sharedInputOutlineStyle}
   }
 `;
 
