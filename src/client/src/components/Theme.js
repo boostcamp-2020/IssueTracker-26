a3962ef35170d8b41e938bfce78e6105f8d3c5c1
chahtk <chahtk@gmail.com>
2020-11-05 15:07:19 +0900

    style: 비밀번호 체크 색상 추가
    - 비밀번호와 비밀번호 확인이 다르면 빨간색, 같으면 기본(파랑)

--- src/client/src/components/Theme.js
+++ src/client/src/components/Theme.js
@@ -1,7 +1,9 @@
 const theme = {
   Color: {
     inputShadow: '#bbd1f3',
-    inputOutline: '#0466d5',
+    warningInputShadow: '#f3acac',
+    inputBorder: '#0466d5',
+    warningInputBorder: '#ff0000',
     border: '#cfd3da',
     lightGreen: '#2c994c',
     blue: '#316FD7',
