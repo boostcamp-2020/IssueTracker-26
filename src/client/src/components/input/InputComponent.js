a2adeab358325f3dff98e3f7d60692b3713dce8d
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-05 11:09:26 +0900

    style: input 컴포넌트 스타일 수정
    - fontSize props로 추가(default : 18px)
    - height props로 추가(default : 42px)
    - focus outline 추가(SharedStyled)

--- src/client/src/components/input/InputComponent.js
+++ src/client/src/components/input/InputComponent.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import SharedStyle from '../SharedStyle';
 
 const InputTag = styled.input`
+  font-size: ${(props) => props.fontSize};
   border: 1px solid ${(props) => props.theme.Color.border};
-  border-radius: 3px;
-  outline-color: ${(props) => props.theme.Color.inputOutline};
-  width: ${(props) => props.width || '150px'};
+  border-radius: 5px;
+  width: ${(props) => props.width};
+  min-height: ${(props) => props.height};
+  padding: 0 0.5rem;
+  ${SharedStyle.inputOutlineStyle}
 `;
 
 function Input(props) {
-  const { type, placeholder, value, onChange, width = '150px' } = props;
+  const {
+    type,
+    placeholder,
+    value,
+    onChange,
+    width = '100%',
+    height = '42px',
+    fontSize = '18px',
+  } = props;
 
   return (
     <InputTag
       width={width}
+      height={height}
+      fontSize={fontSize}
       type={type}
       placeholder={placeholder}
       value={value}
@@ -29,6 +43,8 @@ Input.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   width: PropTypes.string,
+  height: PropTypes.string,
+  fontSize: PropTypes.string,
 };
 
 export default Input;
