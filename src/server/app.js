38a7bc39fac5b58fe5eae12579f88962662861f7
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-05 16:48:10 +0900

    chore: cors 추가

--- src/server/app.js
+++ src/server/app.js
@@ -3,12 +3,14 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const cors = require('cors');
 const initPassport = require('./config/passportConfig');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
