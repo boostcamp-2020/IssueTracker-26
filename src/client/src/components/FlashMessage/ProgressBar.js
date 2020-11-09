79794089c5e1b7a0ee9920da221b3445ef4bb570
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-10 01:05:29 +0900

    feat: FlashMessage의 progressbar 컴포넌트

---
+++ src/client/src/components/FlashMessage/ProgressBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import flashKeyframes from './keyframes';
+
+const Container = styled.div`
+  height: 2%;
+  background-color: ${(props) => props.theme.Color.lightGreen};
+  transform-origin: left;
+  animation: ${flashKeyframes.trackProgress} linear
+    ${(props) => `${props.duration}s`} forwards;
+`;
+
+function ProgressBar({ duration }) {
+  return <Container duration={duration}></Container>;
+}
+
+ProgressBar.PropTypes = {
+  duration: PropTypes.number.isRequired,
+};
+
+export default ProgressBar;
