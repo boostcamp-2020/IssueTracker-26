2738ab048f2831b8846116d7bc9dfe449c168823
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-10 20:20:59 +0900

    feat: presenter, container 구조를 위한 파일 생성

---
+++ src/client/src/components/IssueDetail/IssueDetailContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import IssueDetailPresenter from './IssueDetailPresenter';
+
+function IssueDetailContainer() {
+  return <IssueDetailPresenter />;
+}
+
+export default IssueDetailContainer;
---
+++ src/client/src/components/IssueDetail/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import IssueDetailContainer from './IssueDetailContainer';
+
+function IssueDetail() {
+  return <IssueDetailContainer />;
+}
+
+export default IssueDetail;
