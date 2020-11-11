d138c6d980ad64cd7d2eb7548fe4eecc09778657
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-11 20:19:19 +0900

    feat: comment 생성 api 추가

--- src/client/src/util/api/comment.js
+++ src/client/src/util/api/comment.js
@@ -11,4 +11,16 @@ commentAPI.updateCommentContent = ({ commentId, issueId, content }) =>
     body: JSON.stringify({ content, issueId }),
   }).then((res) => res.status);
 
+commentAPI.createComment = ({ issueId, userId, content }) =>
+  fetch(`${HTTP}api/comment`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ issueId, userId, content }),
+  }).then((res) => {
+    if (res.status === 201) return res.json();
+    return {};
+  });
+
 export default commentAPI;
