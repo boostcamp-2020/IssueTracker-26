ea50906c4b539c2f4961a565aa2d8e6160465fdf
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-11 10:05:56 +0900

    feat: 스타일이 반복되는 컴포넌트는 빼서 관리

--- src/client/src/components/shared/sharedComponents.js
+++ src/client/src/components/shared/sharedComponents.js
@@ -6,4 +6,10 @@ Components.Span = styled.span`
   color: #7f7f7f;
 `;
 
+Components.Button = styled.button`
+  padding: 0.5rem 0.7rem;
+  border-radius: 8px;
+  outline: none;
+`;
+
 export default Components;
--- src/client/src/util/api/issue.js
+++ src/client/src/util/api/issue.js
@@ -13,4 +13,51 @@ issueAPI.getIssue = (id, token) =>
     return {};
   });
 
+issueAPI.updateMilestone = (issueId, milestoneId) =>
+  fetch(`${HTTP}api/issue/milestone/${issueId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ milestoneId }),
+  }).then((res) => {
+    if (res.status === 200) return res.json();
+    return {};
+  });
+
+issueAPI.updateLabelList = (issueId, labelList) =>
+  fetch(`${HTTP}api/issue/labels/${issueId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ labels: labelList }),
+  }).then((res) => res.status);
+
+issueAPI.updateAssigneeList = (issueId, assigneeList) =>
+  fetch(`${HTTP}api/issue/assignees/${issueId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ assignees: assigneeList }),
+  }).then((res) => res.status);
+
+issueAPI.updateIssueTitle = (issueId, title) =>
+  fetch(`${HTTP}api/issue/title/${issueId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title }),
+  }).then((res) => res.status);
+
+issueAPI.updateIssueContent = (issueId, content) =>
+  fetch(`${HTTP}api/issue/content/${issueId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ content }),
+  }).then((res) => res.status);
 export default issueAPI;
