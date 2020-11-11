629e71f017785bacc2e392bb1a485fad8ffd0296
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-11 10:12:06 +0900

    feat: IssueDetailContainer에서 context 설정

--- src/client/src/components/IssueDetail/IssueDetailContainer.js
+++ src/client/src/components/IssueDetail/IssueDetailContainer.js
@@ -1,8 +1,69 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import PropTypes from 'prop-types';
 import IssueDetailPresenter from './IssueDetailPresenter';
+import IssueDetailReducer from './reducer';
+import TransferTime from '../../util/time';
+import IssueDetailContext from '../Context/IssueDetailContext';
 
-function IssueDetailContainer() {
-  return <IssueDetailPresenter />;
+function IssueDetailContainer({ issue }) {
+  const {
+    id,
+    title,
+    content,
+    username,
+    profile,
+    user_id: userId,
+    state: issueState,
+    createdat: createdAt,
+    milestone,
+    assignee,
+    label: labelList,
+  } = issue;
+  const label = labelList.map(
+    ({ labelid, title: labelTitle, description, color }) => ({
+      id: labelid,
+      title: labelTitle,
+      description,
+      color,
+    }),
+  );
+  const milestoneObj = milestone.length
+    ? {
+        id: milestone[0].id,
+        title: milestone[0].title,
+        ratio: milestone[0].ratio,
+      }
+    : { id: undefined, title: undefined, ratio: undefined };
+  const initialState = {
+    issue: {
+      id,
+      title,
+      content,
+      state: issueState,
+      time: TransferTime(createdAt),
+    },
+    user: {
+      id: userId,
+      name: username,
+      profile,
+    },
+    label,
+    assignee,
+    milestone: milestoneObj,
+    input: {
+      value: '',
+    },
+  };
+  const [state, dispatch] = useReducer(IssueDetailReducer, initialState);
+  return (
+    <IssueDetailContext.Provider value={{ state, dispatch }}>
+      <IssueDetailPresenter issue={issue} />
+    </IssueDetailContext.Provider>
+  );
 }
+
+IssueDetailContainer.propTypes = {
+  issue: PropTypes.object,
+};
 
 export default IssueDetailContainer;
--- src/client/src/components/IssueDetail/index.js
+++ src/client/src/components/IssueDetail/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import IssueDetailContainer from './IssueDetailContainer';
 
-function IssueDetail() {
-  return <IssueDetailContainer />;
+function IssueDetail({ issue }) {
+  return <IssueDetailContainer issue={issue} />;
 }
+
+IssueDetail.propTypes = {
+  issue: PropTypes.object,
+};
 
 export default IssueDetail;
