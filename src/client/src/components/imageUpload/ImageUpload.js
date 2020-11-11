bcd9499c5107a317c96f9e00bc8e1bc28c3eb8e5
chahtk <chahtk@gmail.com>
2020-11-11 19:21:12 +0900

    feat: 이미지 업로드 컴포넌트
    - 버튼은 opacity: 0으로 숨김
    - 영역 위에서 커서 바뀜
    - 클릭시 파일 추가 가능

---
+++ src/client/src/components/imageUpload/ImageUpload.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+import theme from '../Theme';
+
+const inputOutlineStyle = css`
+  box-shadow: 0 4px 0 4px ${theme.Color.inputShadow};
+  outline-style: none;
+  border: 0.1px solid ${theme.Color.inputBorder};
+  border-top: 0.1px dotted ${theme.Color.inputBorder};
+`;
+
+const InputImgFileContainer = styled.div`
+  margin-top: -6px;
+  border: 1px solid #e1e4e8;
+  border-top: 1px dotted #e1e4e8;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  width: ${(props) => props.width};
+  height: 30px;
+  position: relative;
+  background: ${theme.Color.lightGrayBackground};
+  ${(props) => props.focus && inputOutlineStyle}
+`;
+
+const DivDescription = styled.div`
+  position: absolute;
+  padding: 6px;
+  padding-left: 10px;
+  color: gray;
+  font-size: 13px;
+`;
+
+const InputImgFile = styled.input`
+  width: 100%;
+  height: 100%;
+  opacity: 0%;
+  position: absolute;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+function ImageUpload(props) {
+  const { width = '100%', handleFiles, focus } = props;
+  return (
+    <InputImgFileContainer width={width} focus={focus}>
+      <DivDescription>
+        Attach files by dragging &amp; dropping, selecting or pasting theme.
+      </DivDescription>
+      <InputImgFile
+        multiple
+        onChange={handleFiles}
+        type="file"
+        accept="image/*"
+      ></InputImgFile>
+    </InputImgFileContainer>
+  );
+}
+
+ImageUpload.propTypes = {
+  width: PropTypes.string,
+  handleFiles: PropTypes.func,
+  focus: PropTypes.bool,
+};
+
+export default ImageUpload;
