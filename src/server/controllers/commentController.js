5668270b98136e4ea3e4fb791cb494d23101d801
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-11 20:14:57 +0900

    feat: comment 생성 후 comment까지 받아서 리턴

--- src/server/controllers/commentController.js
+++ src/server/controllers/commentController.js
@@ -6,9 +6,9 @@ const create = async (req, res) => {
   if (!(content && userId && issueId)) {
     return res.status(400).end();
   }
-  const commentId = await commentService.create({ content, userId, issueId });
-  if (commentId) {
-    return res.status(201).end();
+  const comment = await commentService.create({ content, userId, issueId });
+  if (comment) {
+    return res.status(201).json({ comment });
   }
   return res.status(500).end();
 };
